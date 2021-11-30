5f88627680026cdbd85418a617da11c2931aa0eb
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-11-30 16:09:17 -0500

    Assert that the Round can contain a deck of cards

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/round');
 
@@ -13,5 +14,16 @@ describe('Round', function() {
   it('should be an instance of Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceOf(Round);
+  });
+
+  it('should contain a deck inside the round', function() {
+    const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    const card2 = new Card(6, "What is an example of a mutator method?", ["sort()", "map()", "join()"], "sort()");
+    const card3 = new Card(20, "forEach() returns an array", ["true", "false"], "false");
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+    expect(round.deck).to.equal(deck);
   })
 });
