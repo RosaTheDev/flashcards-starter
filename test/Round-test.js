c8f1250885107c82dab4057af1410c77ed0336c8
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-11-30 16:00:16 -0500

    Create a test asserting that Round is a function

---
+++ test/Round-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Round = require('../src/round');
+
+describe('Round', function() {
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  })
+});
