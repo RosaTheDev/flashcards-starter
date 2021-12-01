70b35e24c4057cbf16fe28bdce706b724fec2705
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-11-30 20:02:58 -0500

    TIt should return a current card

--- src/round.js
+++ src/round.js
@@ -1,6 +1,14 @@
 class Round {
   constructor(deck) {
     this.deck = deck;
+    this.currentCard = {};
+    round 
+  }
+
+  returnCurrentCard() {
+    this.currentCard = this.deck.cards.shift();
+
+    return this.currentCard;
   }
 }
 module.exports = Round;
