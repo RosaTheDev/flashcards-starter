561a9c9ff25bf45ef63d26fb04479d3c8fe6492a
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-11-30 21:48:13 -0500

    Refactor Deck to get all the data objects from the data file

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,11 +3,11 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
+const data = require('../src/data');
 
 describe('Deck', function() {
 
   it('should be a function', function() {
-    const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
@@ -17,12 +17,10 @@ describe('Deck', function() {
   });
 
   it('should have card in the deck', function() {
-    const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-    const card2 = new Card(6, "What is an example of a mutator method?", ["sort()", "map()", "join()"], "sort()");
-    const card3 = new Card(20, "forEach() returns an array", ["true", "false"], "false");
 
-    const deck = new Deck([card1, card2, card3]);
 
-    expect(deck.countCards()).to.equal(3);
+    const deck = new Deck(data.prototypeData);
+
+    expect(deck.countCards()).to.equal(30);
   })
 })
