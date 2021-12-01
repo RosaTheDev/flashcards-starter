b1aff00866f07ae16fc36d67a57511bc25fa63e3
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-11-30 20:05:03 -0500

    Assert that the turns are 0 by default

--- test/Round-test.js
+++ test/Round-test.js
@@ -36,8 +36,18 @@ describe('Round', function() {
     const round = new Round(deck);
 
     expect(round.returnCurrentCard()).to.equal(card1);
-
   });
+
+  it('should have the turns set to 0 by default', function() {
+    const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    const card2 = new Card(6, "What is an example of a mutator method?", ["sort()", "map()", "join()"], "sort()");
+    const card3 = new Card(20, "forEach() returns an array", ["true", "false"], "false");
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck)
+
+    expect(round.turns).to.equal(0);
+  })
 })
 
   
