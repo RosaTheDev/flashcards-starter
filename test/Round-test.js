c69d0375e1b758b60dda996fed229ec6f5dc586b
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-11-30 20:13:07 -0500

    taurns count is updated when takeTurn function is invoked

--- test/Round-test.js
+++ test/Round-test.js
@@ -59,6 +59,19 @@ describe('Round', function() {
 
     expect(round.incorrectGuesses).to.eql([]);
   })
+
+  it('should update the turns count when takeTurn method is invoked', function() {
+    const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    const card2 = new Card(6, "What is an example of a mutator method?", ["sort()", "map()", "join()"], "sort()");
+    const card3 = new Card(20, "forEach() returns an array", ["true", "false"], "false");
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    
+    round.takeTurn();
+
+    expect(round.turns).to.equal(1);
+  })
 })
 
   
