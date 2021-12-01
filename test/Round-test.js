71e3174076f6e53e7332be75d319005f66745bc6
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-11-30 20:08:21 -0500

    assert incorrectGuesses is an empty array

--- test/Round-test.js
+++ test/Round-test.js
@@ -44,9 +44,20 @@ describe('Round', function() {
     const card3 = new Card(20, "forEach() returns an array", ["true", "false"], "false");
 
     const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck)
+    const round = new Round(deck);
 
     expect(round.turns).to.equal(0);
+  });
+
+  it('should have incorrectGuesses set to an empty array', function() {
+    const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    const card2 = new Card(6, "What is an example of a mutator method?", ["sort()", "map()", "join()"], "sort()");
+    const card3 = new Card(20, "forEach() returns an array", ["true", "false"], "false");
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.incorrectGuesses).to.equal([]);
   })
 })
 
